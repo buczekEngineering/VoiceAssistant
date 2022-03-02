2a8aa1d195b388ad1d52b7528f3595949b29bebc
Sylwia-softtdev <sylwiabuczek@hotmail.com>
2022-03-02 09:44:58 +0100

    refactored, created a config file based setup, setting dynamically voice based on language

--- TTS.py
+++ TTS.py
@@ -3,11 +3,13 @@ import time
 import pyttsx3
 import multiprocessing
 
+
 def __speak__(text, voice_id):
     engine = pyttsx3.init()
     engine.setProperty('voice', voice_id)
     engine.say(text)
     engine.runAndWait()
+
 
 class Voice:
 
@@ -32,10 +34,13 @@ class Voice:
     def get_voice_keys_for_language(self, language=""):
         result = list()
         engine = pyttsx3.init()
+        lang_search_string = language.upper() + "_"
+        logger.info(f"LANGUAGE SEARCH STRING: {lang_search_string}")
         voices = engine.getProperty('voices')
         for voice in voices:
             if language == "":
                 result.append(voice.id)
-            elif language.lower() in voice.name.lower():
+            elif lang_search_string in voice.id:
+                logger.info("VOICE FOUND!")
                 result.append(voice.id)
         return result
--- voice_assistant.py
+++ voice_assistant.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import yaml
 import sys
 import multiprocessing
 from TTS import Voice
@@ -7,16 +8,35 @@ from loguru import logger
 logger.remove()
 logger.add(sys.stdout, level="DEBUG")
 
+CONFIG_FILE = 'config.yml'
+
 
 class VoiceAssistant():
     def __init__(self):
+
+        global CONFIG_FILE
+        with open(CONFIG_FILE, "r", encoding="utf-8")as yamlfile:
+            self.cfg = yaml.load(yamlfile, Loader=yaml.FullLoader)
+            print(self.cfg)
+        if self.cfg:
+            logger.debug("Config file found and applied.")
+        else:
+            logger.error("No config file found.")
+
+        language = self.cfg["assistant"]["language"]
+        if not language:
+            language = "de"
+        logger.info(f"Used language: {language}")
         logger.debug("Voice assistant is initiaized.")
         self.tts = Voice()
-        voices = self.tts.get_voice_keys_for_language(language="")
+        voice = self.tts.get_voice_keys_for_language(language=language)
+        logger.info(f"Choosed voice: {voice}")
+        if len(voice) > 0:
+            self.tts.set_voice(voice[0])
 
     def run(self):
         logger.info("Starting the assistant...")
-        self.tts.say("Hallo Sylwia, ich bin Tommy, deín Sprachassistent !")
+        self.tts.say("Hallo Sylwia, ich bin Tommy, deín Sprachassistent!")
 
 
 if __name__ == "__main__":
